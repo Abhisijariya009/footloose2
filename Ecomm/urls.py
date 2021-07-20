41a9e007c16cb094d074a628d1669c9fee3a21e2
Abhishek Sijariya <87616885+Abhisijariya009@users.noreply.github.com>
2021-07-20 13:02:00 +0530

    Update urls.py

--- Ecomm/urls.py
+++ Ecomm/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     # path('base/',views.base,name = 'base')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
