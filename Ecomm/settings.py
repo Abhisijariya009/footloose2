be624814d79fad20badaf9a27fdbfb61c0d6a4cf
Abhishek Sijariya <87616885+Abhisijariya009@users.noreply.github.com>
2021-07-24 02:39:32 +0530

    Update settings.py

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -141,7 +141,8 @@ AWS_STORAGE_BUCKET_NAME = 'footloose-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+#AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = False
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
