45b0b113b251fbada6d966110c56497cb59ddded
Abhishek Sijariya <87616885+Abhisijariya009@users.noreply.github.com>
2021-07-24 02:25:30 +0530

    Update settings.py

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -142,7 +142,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
